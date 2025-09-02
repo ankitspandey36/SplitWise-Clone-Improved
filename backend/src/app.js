b577bec0018d4e2a92494c69f57d852ec7969cc8
ankitspandey36 <ankit.pandey99p@gmail.com>
2025-09-02 13:59:22 +0530

    Links

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://split-wise-clone-improved.vercel.app/",
     credentials: true
 }))
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"],
+        origin: ["http://localhost:5173","https://split-wise-clone-improved.vercel.app/"],
         methods: ["GET", "POST"],
         credentials: true,
     },
